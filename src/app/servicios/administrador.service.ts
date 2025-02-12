7c5f0df3f391d3a8cc011d5ecfd80b68e156d6b9
Sebastián Ramos <167812683+Developer1Emo@users.noreply.github.com>
2025-02-12 10:57:00 -0500

    Logica en el componente CrearUsuario
    Se crea la lógica para el componente CrearUsuario.

---
+++ src/app/servicios/administrador.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MensajeDTO } from '../dto/mensaje-dto';
+import { Observable } from 'rxjs';
+import { CrearUsuarioDTO } from '../dto/crear-usuario-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdministradorService {
+
+
+ private admintURL = "http://localhost:8080/administrator";
+
+ constructor(private http: HttpClient) {  }
+
+ public crearUsuario(crearUser: CrearUsuarioDTO ): Observable<MensajeDTO> {
+  return this.http.post<MensajeDTO>(`${this.admintURL}/usuarios/crearUsuarios/`, crearUser);
+ }
+}
